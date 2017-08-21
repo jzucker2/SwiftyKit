e2d59e5a70bda3d75f3513aad284b7c2e168dd03
Jordan Zucker <jordan.zucker@gmail.com>
2017-08-21 11:00:46 -0700

    Minor adjustment to command line args for travis

--- SwiftyKit/Classes/StackingView.swift
+++ SwiftyKit/Classes/StackingView.swift
@@ -44,7 +44,7 @@ extension StackingView {
         return StackViewOptions.defaultOptions()
     }
     
-    init?(coder aDecoder: NSCoder) {
+    public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
