198483c322ec9b608712e56514cf45b9143ba3a6
Jordan Zucker <jordan.zucker@gmail.com>
2018-01-08 09:06:32 -0800

    Added some extra UIKit extensions with no tests yet

--- SwiftyKit/Classes/UserInterfaceExtensions.swift
+++ SwiftyKit/Classes/UserInterfaceExtensions.swift
@@ -11,6 +11,55 @@ import UIKit
 
 public let NavigationBarPromptDefaultDuration = 1.5
 
+// http://stackoverflow.com/questions/26542035/create-uiimage-with-solid-color-in-swift
+extension UIImage {
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
+
+extension UIDevice {
+    
+    public var isIPhone: Bool {
+        return userInterfaceIdiom == .phone
+    }
+    
+    public var isIPad: Bool {
+        return userInterfaceIdiom == .pad
+    }
+    
+    public var isTV: Bool {
+        return userInterfaceIdiom == .tv
+    }
+    
+}
+
+extension UIView {
+    public func roundCorners() {
+        // There are more efficient ways to do this,
+        // I should be drawing a UIBezierPath
+        // TODO: Clean this up
+        layer.masksToBounds = true
+        layer.cornerRadius = 5.0
+    }
+}
+
+extension UIControl {
+    public func removeAllTargets() {
+        self.allTargets.forEach { (target) in
+            self.removeTarget(target, action: nil, for: .allEvents)
+        }
+    }
+}
+
 extension UIView {
     
     public static func reuseIdentifier() -> String {
@@ -20,6 +69,12 @@ extension UIView {
 }
 
 extension UIView {
+    
+    public var hasConstraints: Bool {
+        let hasHorizontalConstraints = !self.constraintsAffectingLayout(for: .horizontal).isEmpty
+        let hasVerticalConstraints = !self.constraintsAffectingLayout(for: .vertical).isEmpty
+        return hasHorizontalConstraints || hasVerticalConstraints
+    }
     
     public func forceAutolayout() {
         translatesAutoresizingMaskIntoConstraints = false
